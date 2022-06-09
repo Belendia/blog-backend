8638d8b28e05b8edb2e6400e0a64e11a3140cebe
Belendia Serda <belendia@gmail.com>
2022-06-09 09:51:17 +0300

    Update post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,7 @@
 const { validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
+
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
@@ -81,4 +84,66 @@ exports.getPost = (req, res, next) => {
       }
       next(err); // will be catched by the global error handler defined in app.js
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = req.params.postId;
+
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image; // if we don't pick image file in the frontend
+
+  // if we picked a file in the frontend
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error; // it will go to the catch block
+      }
+
+      // if a new file is uploaded, then remove the old file
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Post updated!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err); // will be catched by the global error handler defined in app.js
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
