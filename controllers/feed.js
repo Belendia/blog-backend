0374713c83b61823bc3a8ba9726cb034fb1afca1
Belendia Serda <belendia@gmail.com>
2022-06-12 15:18:23 +0300

    Add authroization checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -17,13 +18,11 @@ exports.getPosts = (req, res, next) => {
         .limit(perPage);
     })
     .then((posts) => {
-      res
-        .status(200)
-        .json({
-          message: "Fetched posts successfully.",
-          posts: posts,
-          totalItems: totalItems,
-        });
+      res.status(200).json({
+        message: "Fetched posts successfully.",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -51,21 +50,31 @@ exports.createPost = (req, res, next) => {
 
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
+
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: "Solomon",
-    },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
-      // Create post in db
+      // we did this because we want to store the post in the user model as well.
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      // store the post inside the user model as well
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((user) => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
@@ -133,6 +142,13 @@ exports.updatePost = (req, res, next) => {
         throw error; // it will go to the catch block
       }
 
+      // a user should update his/her own post
+      if (post.creator.toString() !== req.userid) {
+        const error = new Error("Not authroized!");
+        error.statusCode = 403;
+        throw error;
+      }
+
       // if a new file is uploaded, then remove the old file
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
@@ -166,6 +182,13 @@ exports.deletePost = (req, res, next) => {
         const error = new Error("Could not find post.");
         error.statusCode = 404;
         throw error; // it will go to the catch block
+      }
+
+      // a user should delete his/her own post
+      if (post.creator.toString() !== req.userid) {
+        const error = new Error("Not authroized!");
+        error.statusCode = 403;
+        throw error;
       }
 
       // Check logged in user
