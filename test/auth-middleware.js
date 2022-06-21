ef2ef505c0aeba361b43c3b48dc7eb8a0b76760f
Belendia Serda <belendia@gmail.com>
2022-06-21 16:08:49 +0300

    Add authorization bearer header test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -7,7 +7,18 @@ it("should throw and error if no authorization header is present", function () {
       return null;
     },
   };
+  // we use bind because we want expect() function to call authMiddleware function and handle errors by its own.
   expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
     "Not authenticated"
   );
+});
+
+it("should throw an error if the authorization header is not one string", function () {
+  const req = {
+    get: function (headerName) {
+      return "xyz";
+    },
+  };
+
+  expect(authMiddleware.bind(this.req, {}, () => {})).to.throw();
 });
